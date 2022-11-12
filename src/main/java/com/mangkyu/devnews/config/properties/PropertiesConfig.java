500903d7a04a72e7591f6f583aed781d10f944d4
MangKyu <whalsrb1226@gmail.com>
2022-11-12 20:19:34 +0900

    Add PropertiesConfig

---
+++ src/main/java/com/mangkyu/devnews/config/properties/PropertiesConfig.java
@@ -0,0 +1,9 @@
+package com.mangkyu.devnews.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan("com.mangkyu.devnews.app")
+public class PropertiesConfig {
+}

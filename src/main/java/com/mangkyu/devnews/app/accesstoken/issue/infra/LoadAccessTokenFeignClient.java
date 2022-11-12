bcaf36fe9fd6a1c37e1222e5086a0574f4744061
MangKyu <whalsrb1226@gmail.com>
2022-11-12 20:33:38 +0900

    BugFix on FeignClient

--- src/main/java/com/mangkyu/devnews/app/accesstoken/issue/infra/LoadAccessTokenFeignClient.java
+++ src/main/java/com/mangkyu/devnews/app/accesstoken/issue/infra/LoadAccessTokenFeignClient.java
@@ -1,12 +1,13 @@
 package com.mangkyu.devnews.app.accesstoken.issue.infra;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(url = "${openapi.url}", name = "LoadAuthCodeFeignClient")
 public interface LoadAccessTokenFeignClient {
 
-    @GetMapping("/oauth/2.0/token")
-    LoadAccessTokenFeignResponse issue(LoadAccessTokenFeignRequest toRequest);
+    @PostMapping("/oauth/2.0/token")
+    LoadAccessTokenFeignResponse issue(@RequestBody LoadAccessTokenFeignRequest toRequest);
 
 }

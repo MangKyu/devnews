5be86ce19395c71c8165c9f11fd946c985694c7c
MangKyu <whalsrb1226@gmail.com>
2022-11-12 17:16:54 +0900

    Initial Commit

---
+++ src/main/java/com/mangkyu/devnews/DevNewsApplication.java
@@ -0,0 +1,13 @@
+package com.mangkyu.devnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevNewsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mangkyu/devnews/DevNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangkyu.devnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevNewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
